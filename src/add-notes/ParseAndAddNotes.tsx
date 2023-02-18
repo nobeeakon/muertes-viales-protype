337d9fe606c9fd4fcaf743ea2372e7bc68e3b9cf
Daniel Torres <nobeeakon@gmail.com>
2023-02-18 08:50:10 -0600

    prettier

--- src/add-notes/ParseAndAddNotes.tsx
+++ src/add-notes/ParseAndAddNotes.tsx
@@ -1,49 +1,48 @@
-import  { useState, useRef} from 'react';
+import { useState, useRef } from "react";
 
-import { dbSaveUrls } from '../firebase/firebase';
-
-
+import { dbSaveUrls } from "../firebase/firebase";
 
 function ParseAndAddNotes() {
-  const [urls, setUrls] = useState<string>('');
-  const inputRef= useRef<HTMLInputElement>(null)
+  const [urls, setUrls] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
 
+  const saveUrls = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dbSaveUrls(urls.split(",")).then(() => {
+      setUrls("");
+      inputRef.current?.focus();
+    });
+  };
 
+  const onUrlsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newUrls = event.target.value
+      .split(",")
+      .map((urlItem) => urlItem.trim())
+      .filter(Boolean);
 
-    const saveUrls = (event:React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      dbSaveUrls(urls.split(',')).then(() => {
-        setUrls('');
-        inputRef.current?.focus()
-      })
-    }
+    const urlsSet = new Set(newUrls);
 
-    const onUrlsChange = (event:  React.ChangeEvent<HTMLInputElement>) => {
-        const newUrls = event.target.value.split(',').map(urlItem => urlItem.trim()).filter(Boolean)
+    setUrls(Array.from(urlsSet).join(","));
+  };
 
-        const urlsSet = new Set(newUrls)
-
-        setUrls(Array.from(urlsSet).join(','))
-    }
-
-    
   return (
-    <div >
+    <div>
       <h2>Agregar notas</h2>
       <div>
-
-    <form onSubmit={saveUrls}>
-       <input
-        ref={inputRef}
-        type='text  '
-        value={urls} onChange={onUrlsChange}/>
-        <button type='submit'>Guardar</button>
+        <form onSubmit={saveUrls}>
+          <input
+            ref={inputRef}
+            type="text  "
+            value={urls}
+            onChange={onUrlsChange}
+          />
+          <button type="submit">Guardar</button>
         </form>
-<ol>
-
-{urls.split(',').length > 0 && urls.split(',').map(urlItem => <li key={urlItem}>{urlItem}</li>)}
-</ol>
-        </div>
+        <ol>
+          {urls.split(",").length > 0 &&
+            urls.split(",").map((urlItem) => <li key={urlItem}>{urlItem}</li>)}
+        </ol>
+      </div>
     </div>
   );
 }
--- src/firebase/types.ts
+++ src/firebase/types.ts
@@ -1,29 +1,33 @@
 export type NoteInfo = {
-    sex: string;
-    victimName: string;
-    date: {
-        year:number;
-        month:number;
-        day:number;
-    };
-    ageRange: string;
-    victimTransport:string;
-    coordinates: {
-        latitude: number;
-        longitude: number;
-    }
-}
+  sex: string;
+  victimName: string;
+  date: {
+    year: number;
+    month: number;
+    day: number;
+  };
+  ageRange: string;
+  victimTransport: string;
+  coordinates: {
+    latitude: number;
+    longitude: number;
+  };
+};
 
 export type NoteInfoFieldsType = keyof NoteInfo;
-export const noteInfoFields:Array<keyof NoteInfo>= ['sex', 'victimName', 'date', 'ageRange',
-    'victimTransport', 'coordinates'
-]
+export const noteInfoFields: Array<keyof NoteInfo> = [
+  "sex",
+  "victimName",
+  "date",
+  "ageRange",
+  "victimTransport",
+  "coordinates",
+];
 
 export type NoteType = {
-    id: string;
-    urls: string[];
-    noteInfo?: Partial<NoteInfo>;
-}
+  id: string;
+  urls: string[];
+  noteInfo?: Partial<NoteInfo>;
+};
 
-export type DataType = Record<string, NoteType>
-
+export type DataType = Record<string, NoteType>;
